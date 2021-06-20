6f8e8fed4b1be97245793fd116d5e48600345dac
nam-nguyen <namnguyen30500@gmail.com>
2021-06-20 16:30:38 +0700

    Release Red-Store

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+var MenuItems=document.getElementById("menuItems");
+MenuItems.style.maxHeight="0px"
+function menutoggle(){
+    if(MenuItems.style.maxHeight=="0px"){
+        MenuItems.style.maxHeight="230px"
+    } else {
+        MenuItems.style.maxHeight="0px"
+    }
+}
+
+// ==================================
+// product-gallery
+var productImg = document.getElementById("productImg");
+var smallImg = document.getElementsByClassName("small-img");
+    smallImg[0].onclick= function(){
+        productImg.src =smallImg[0].src;
+    }
+    smallImg[1].onclick= function(){
+        productImg.src=smallImg[1].src;
+    }
+    smallImg[2].onclick= function(){
+        productImg.src=smallImg[2].src;
+    }
+    smallImg[3].onclick= function(){
+        productImg.src=smallImg[3].src;
+    }
+
+// =============================
+// Toggle Form 
+
+
